dd2cb6ed3a1859773b16e95bafb64057cbb5f2d5
Andrei Ilin <a.ilin@innopolis.university>
2026-09-17 10:59:35 +0300

    Improve scheduler Docker startup

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,14 @@ import time
 
 while True:
     print("Running DVC pipeline...")
-    subprocess.run(["dvc", "repro"])
+    subprocess.run(["dvc", "repro"], check=True)
+
+    print("Ensuring Docker services are running...")
+    subprocess.run([
+        "docker", "compose",
+        "-f", "code/deployment/docker-compose.yml",
+        "up", "-d"
+    ], check=True)
+
     print("Next run in 5 minutes.\n")
     time.sleep(300)
